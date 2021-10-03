55be079e903bac26f303b78834f57ae3ad1e326d
Raven <vale.windows13@gmail.com>
2021-10-03 13:52:57 -0700

    Constant.

---
+++ Basics/constant.js
@@ -0,0 +1,18 @@
+
+// When you log this you will see the new value 
+let interestRate = 0.13;
+interestRate = 1;
+console.log(interestRate);
+// 1 
+
+// Value of variables can be change, 
+// but Value of constant cannot change.
+
+// You'll see an error once you run this code:
+const interestRate = 0.13;
+interestRate = 1;
+console.log(interestRate);
+// Error
+
+// Best Practice: If you don't need to re assign 'const' is your best choice
+// Otherwise use 'let'
