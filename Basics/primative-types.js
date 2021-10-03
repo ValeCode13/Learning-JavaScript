af7b4a9674190521c29387d55f8d5cc8a174355f
Raven <vale.windows13@gmail.com>
2021-10-03 14:02:54 -0700

    Primative Types.

---
+++ Basics/primative-types.js
@@ -0,0 +1,20 @@
+// Two Category of types:
+// 1.Primative/Value types
+// - Strings
+// - Number
+// - Boolean
+// - undefined
+// - null
+
+let name = 'Oliver'; // String Literals
+let age = 25; // Number Literals
+let isApproved = true; // Boolean Literals - use in logic.
+let firstName; // Undefined - You can explicitly defined it >> let firstName = undefined;
+let lastName = null; // Use to explicitly clear the value of variable
+
+// null - if the user didn't pick a color the value will be null;
+let selectedColor = null;
+console.log(name);
+
+
+// 2. Reference types
