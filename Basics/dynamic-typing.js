156a202a8a1fabdfc2813984162af771f8dafa43
Raven <vale.windows13@gmail.com>
2021-10-03 14:13:43 -0700

    Dynamic Typing.

---
+++ Basics/dynamic-typing.js
@@ -0,0 +1,22 @@
+// JS is a Dynamic Language
+// Two types of Progamming Languages:
+// 1. Static Languages - the variables is set and cannot be change in the future.
+// string name = 'John';
+
+// 2. Dynamic Languages - type of varible can change at run time
+let name = 'Oliver'; // String Literals
+let age = 25; // Number Literals
+let isApproved = true; // Boolean Literals - use in logic.
+let firstName; // Undefined - You can explicitly defined it >> let firstName = undefined;
+let lastName = null; // Use to explicitly clear the value of variable
+
+// We use 'typeof' operator in console to check the type of varible.
+// typeof name 
+// 'string'
+// name = 13
+// typeof name
+// 'number'
+
+// JS doesn't have floating numbers and integers
+// undefined - is not only a type but also a value.
+// null - is an "object"
