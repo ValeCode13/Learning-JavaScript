58e81f91c29bce377f3c982c8d80fe61326fef7e
Raven <vale.windows13@gmail.com>
2021-10-04 03:33:33 -0700

    Arrays.

---
+++ Basics/arrays.js
@@ -0,0 +1,28 @@
+// Arrays - use array to store list of abjects.
+// Arrays - is a data structure that we use to represent list of items.
+// [] - this is array literal
+
+// Index - use this to access an elements inside an array ( starts from 0 )
+// let selectedColors = ['red', 'blue'];
+// console.log(selectedColors[0]) - Result: 'red'
+
+// Just like JS, array is also dynamic:
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'gold';
+// console.log(selectedColors)
+
+// In JS you can store differents types inside an array:
+// let selectedColors = ['red', 'blue', 22];
+// selectedColors[3] = 13;
+// console.log(selectedColors)
+
+// Arrays is an Object As well it has a lot properties that you can use just like 'dot notation':
+// typeof selectedColors > "obejct"
+// let selectedColors = ['red', 'blue', 22];
+// selectedColors[3] = 13;
+// console.log(selectedColors.length)
+
+
+let selectedColors = ['red', 'blue', 22];
+selectedColors[3] = 13;
+console.log(selectedColors.length)
