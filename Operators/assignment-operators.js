ed7f528c78bf4ceac4269c307a0328395f880bd8
Raven <vale.windows13@gmail.com>
2021-10-04 06:38:53 -0700

    Assignment Operators.

---
+++ Operators/assignment-operators.js
@@ -0,0 +1,19 @@
+//  Basic Assignment Operator - 'assigning' 10 to the variable x.
+let x = 10;
+
+x++; // with this we increment the value of x by 1.
+x = x + 1;
+
+// Addition Operator:
+x = x + 5;
+x += 5;
+
+//Multiplication Operator: 
+x = x * 3;
+x *= 3;
+
+// Combination of Assignment Operator:
+x += 13;
+x -= 13:
+x *= 13;
+x /= 13;
