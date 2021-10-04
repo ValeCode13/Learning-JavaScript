9247d313f6ad61c36c8f97b6964de90e0517720c
Raven <vale.windows13@gmail.com>
2021-10-04 06:48:14 -0700

    Comparison Operators.

---
+++ Operators/comparison-operators.js
@@ -0,0 +1,15 @@
+// Comparison Operator - we use this to compare value of varible with something else.
+
+
+// The result of expression with a 'comparison operator' is 'boolean' - True or False.
+let x = 1;
+
+// Relational Operators:
+console.log(x > 0);
+console.log(x >= 1); // True
+console.log(x < 1); // False
+console.log(x <= 1); // False
+
+// Equality Operators:
+console.log (x === 1); // to checked if x is equal to one.
+console.log (x !== 1); // to checked if x is 'not' equal to one.
