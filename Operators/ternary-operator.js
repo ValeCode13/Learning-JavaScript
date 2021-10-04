cc22c219b16dbc9afc65d2e2a21018cdaf30819b
Rjay <vale.windows13@gmail.com>
2021-10-04 07:18:56 -0700

    Ternary Operator.

---
+++ Operators/ternary-operator.js
@@ -0,0 +1,12 @@
+// If the customer has more than 100 points, 
+// they are a 'gold' customer. Otherwise,
+// they are 'silver' customer.
+
+// This expression 'points > 100' produces a boolean.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log('You are a ' + type + ' ' + 'customer');
+
+
