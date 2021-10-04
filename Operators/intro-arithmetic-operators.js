f75b7345c0c29a593e39b51f0976a88c43115bd5
Raven <vale.windows13@gmail.com>
2021-10-04 05:56:14 -0700

    Arithmetic Operator.

---
+++ Operators/intro-arithmetic-operators.js
@@ -0,0 +1,36 @@
+// We use operators along  with variables and constant
+// To Create expressions.
+//  With these expressions we can create Logic and Algorithms.
+
+// Types of Operators in JS:
+// 1. Arithmetic
+// 2. Assignment
+// 3. COmparison
+// 4. Logical
+// 5. Bitwise
+
+// Arithmetic Opetor - we use this to perform a calculation just like we have in mathematics.
+
+// Expression - is something that produces a value.
+// (x + y) - is called an expression in JS
+console.log (x + y); 
+console.log (x - y); 
+console.log (x * y); 
+console.log (x / y); 
+console.log (x % y);  // Remainder of Division
+console.log (x ** y); // New Operator it's called "Exponentiation" - x to the power of y
+
+let x = 10;
+let y = 25;
+// Increment and Decrement Operators:
+// 1. Increment (++) - value will be different based on how you position this value
+//  console.log (++x); //11
+
+console.log (x++); // 10
+console.log(x); // 11
+
+// 2. Decrament (--)
+console.log(--x); // 9. The answer is 9 Because we apply the operator first then the variable.
+
+
+
