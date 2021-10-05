119c136348b0056fe4dd6c5208fdb84bbb27d4e9
Rjay <vale.windows13@gmail.com>
2021-10-05 04:27:38 -0700

    Logical Operators - Modified.

--- Operators/logical-operators.js
+++ Operators/logical-operators.js
@@ -1,26 +1,26 @@
-// Logical Operator - this operator makes decision based on multiple conditions.
+// // Logical Operator - this operator makes decision based on multiple conditions.
 
-// Three types of JavaScript Operators:
-// 1. Logical AND (&&)
-// 2. Logical OR (||)
-// 3. Logical NOT ()
+// // Three types of JavaScript Operators:
+// // 1. Logical AND (&&)
+// // 2. Logical OR (||)
+// // 3. Logical NOT ()
 
-// 1. Logical AND (&&)
-// Returns True if both operands are True.
+// // 1. Logical AND (&&)
+// // Returns True if both operands are True.
 console.log(true && true); // true
 console.log(false && true); // false
 
 
-// Application for approving loans, Checking the eligibilty.
-// Make sure that the applicant have a high income and good credit score.
+// // Application for approving loans, Checking the eligibilty.
+// // Make sure that the applicant have a high income and good credit score.
 let highIncome = true;
 let goodCreditScore = true;
 let eligibleForLoan = highIncome && goodCreditScore;
 console.log(eligibleForLoan); // True
 
-// 2. Logical OR (||)
-// Returns TRUE if one of the operands is TRUE.
-// As long as you have 1 true operand the result will be TRUE.
+// // 2. Logical OR (||)
+// // Returns TRUE if one of the operands is TRUE.
+// // As long as you have 1 true operand the result will be TRUE.
 let highIncome = false;
 let goodCreditScore = true;
 let eligibleForLoan = highIncome || goodCreditScore;
@@ -33,7 +33,8 @@ let goodCreditScore = false;
 let eligibleForLoan = highIncome || goodCreditScore;
 
 // if the eligibleForLoan is true, ! operator will convert it to false
-let applicationRefused = !eligibleForLoan;
+//  But if the eligible for loans is false !(not) operator will convert it to 'True"
+let applicationRefused = !eligibleForLoan; // This is False but ! operator turn it to True.
 
-console.log('Eligible', eligibleForLoan);
-console.log('Refused', applicationRefused);
+console.log('Eligible', eligibleForLoan); // False
+console.log('Refused', applicationRefused); // True
